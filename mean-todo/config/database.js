d71575fc8aa87ecbe6525ccad196ae9f88bb2370
mirnas1 <ameernas@udel.edu>
2026-04-10 10:13:46 -0400

    Update database.js

--- mean-todo/config/database.js
+++ mean-todo/config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/mean-todo',
-    secret: 'balls'
+    database: process.env.MONGODB_URI || 'mongodb://localhost:27017/mean-todo',
+    secret: process.env.JWT_SECRET || 'balls'
 }
